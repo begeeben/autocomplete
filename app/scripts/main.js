65fc08699191a4fa9874b3ad74020897f9a1f043
Yi-Fan Liao <begeeben@gmail.com>
2014-05-06 00:41:58 +0800

    Add circular reference and demo code.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
+'use strict';
+
 console.log('\'Allo \'Allo!');
+
+circular.Module('ajax')({url: 'dataset/tz.json', async: true}).then(function (data) {
+	console.log('then!!!!!');
+	console.log(data);
+});
