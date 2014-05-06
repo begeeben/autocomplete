8de94d91a69e945935e6404aeb3172e09477442b
Yi-Fan Liao <begeeben@gmail.com>
2014-05-06 22:03:42 +0800

    Add class manipulation methods.

--- app/scripts/circular.js
+++ app/scripts/circular.js
@@ -48,6 +48,39 @@
         return modulesCache[name];
     }
 
+    function addClass(el, className) {
+        if (el.classList) {
+            el.classList.add(className);
+        } else {
+            el.className += ' ' + className;
+        }
+    }
+
+    function removeClass(el, className) {
+        if (el.classList) {
+            el.classList.remove(className);
+        } else {
+            el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+    }
+
+    function toggleClass(el, className) {
+        if (el.classList) {
+            el.classList.toggle(className);
+        } else {
+            var classes = el.className.split(' ');
+            var existingIndex = classes.indexOf(className);
+
+            if (existingIndex >= 0) {
+                classes.splice(existingIndex, 1);
+            } else {
+                classes.push(className);
+            }
+
+            el.className = classes.join(' ');
+        }
+    }
+
     // shallow extend
     circular.extend = function (target, source) {
         // check if source is a object
