b58b80f15612d04456056e86426bbf25664d748e
Yi-Fan Liao <begeeben@gmail.com>
2014-05-08 21:29:41 +0800

    Add dataset folder in build path.

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,7 +285,8 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
-                        'styles/fonts/{,*/}*.*'
+                        'styles/fonts/{,*/}*.*',
+                        'dataset/*.json'
                     ]
                 }]
             },
